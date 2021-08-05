833be0ac1d51ca8b2d4b01f9bc83f6dde0de539d
Noga Jacobi <nogajacobi@gmail.com>
2021-08-05 14:27:51 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login'
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Login/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

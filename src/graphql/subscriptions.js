3d4ef81370647256ac871be2da8fb220465a1f19
BZakro <bennyzakro@gmail.com>
2021-08-05 15:23:10 +0300

    first commit

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      username
+      password
+      email
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      username
+      password
+      email
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      username
+      password
+      email
+      createdAt
+      updatedAt
+    }
+  }
+`;
